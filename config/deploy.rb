dfc7c27a03372a7c35b7be706ce889bc693868e3
Chris Allick <chrisallick@gmail.com>
2013-03-20 18:27:06 -0700

    modified deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,1 +1,10 @@
 set :application, "touch"
+
+set :scm, :git
+set :repository, "git@github.com:GSPBetaGroup/RubyWSServer.git"
+set :scm_passphrase, ""
+
+set :user, "ubuntu"
+
+server "54.245.111.204", :app, :primary => true
+set :deploy_to, "/home/ubuntu/RubyWSServer"
