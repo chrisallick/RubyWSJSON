16acdde79353152cb82544bb485498e026eefc38
Chris Allick <chrisallick@gmail.com>
2013-03-20 18:42:26 -0700

    modified deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,6 @@ set :scm_passphrase, ""
 set :user, "ubuntu"
 
 server "54.245.111.204", :app, :primary => true
-set :deploy_to, "/var/www/RubyWSServer"
+set :deploy_to, "/home/ubuntu/RubyWSServer"
 
 set :normalize_asset_timestamps, false
